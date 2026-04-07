e1dc1e516b0037a0ae755a5a20ec88785c374a91
Junior Antunes <jugabriel2612@gmail.com>
2026-04-07 07:52:07 -0300

    Feat: Connection

--- src/main/java/org/example/database/Conexao.java
+++ src/main/java/org/example/database/Conexao.java
@@ -5,12 +5,29 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static String URL = "jdbc:mysql://localhost:3306/simulado?useSSL=false&serverTimezone=UTC";
+    private static String URL = "jdbc:postgresql://localhost:5456/gestao_qualidade";
 
-    private static String USER = "root";
+    private static String USER = "junior";
 
-    private static String PASSWORD = "mysqlPW";
+    private static String PASSWORD = "junior";
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL,USER,PASSWORD);
     }
 }
+
+
+//package org.example.util;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class ConexaoBanco {
+//    private static final String URL = "jdbc:postgresql://localhost:5456/java_com_testes";
+//    private static final String USER = "junior";
+//    private static final String PASSWORD = "junior";
+//
+//    public static Connection conectar() throws SQLException {
+//        return DriverManager.getConnection(URL, USER, PASSWORD);
+//    }
+//}
